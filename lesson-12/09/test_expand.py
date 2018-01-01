894696d3fd40e8fd71dd41c83f57dd8e5589eab4
Joost Aafjes <joostaafjes@gmail.com>
2018-01-01 17:01:27 +0100

    add lesson 9

--- lesson-12/09/test_expand.py
+++ lesson-12/09/test_expand.py
@@ -1,4 +1,4 @@
-from first_search_program import search, get_list_from_node, extend_list
+from expansion_grid import search, get_list_from_node, extend_list
 
 def test_1():
     # Grid format:
@@ -14,7 +14,7 @@ def test_1():
     goal = [len(grid)-1, len(grid[0])-1]
     cost = 1
 
-    assert search(grid, init, goal, cost) == [11, 4, 5]
+    print search(grid, init, goal, cost)
 
 def test_2():
     # Grid format:
@@ -30,7 +30,7 @@ def test_2():
     goal = [len(grid)-1, len(grid[0])-1]
     cost = 1
 
-    assert search(grid, init, goal, cost) == [9, 4, 5]
+    print search(grid, init, goal, cost)
 
 def test_3():
     # Grid format:
@@ -46,7 +46,7 @@ def test_3():
     goal = [len(grid)-1, len(grid[0])-1]
     cost = 1
 
-    assert search(grid, init, goal, cost) == [15, 4, 5]
+    print search(grid, init, goal, cost)
 
 def test_4():
     # Grid format:
@@ -59,53 +59,36 @@ def test_4():
     goal = [len(grid)-1, len(grid[0])-1]
     cost = 1
 
-    assert search(grid, init, goal, cost) == [2, 1, 1]
+    print search(grid, init, goal, cost)
 
-def test_one_cell_that_fails():
-    grid = [[0]]
-    list = [0, 0, 0]
-    goal = [len(grid)-1, len(grid[0])-1]
-    cost = 1
-
-    assert get_list_from_node(list, grid, 0, 1) == []
-
-def test_extend_1():
-    assert extend_list([[0, 0, 0]], [[1, 0, 0]]) == [[0, 0, 0]]
-
-def test_extend_2():
-    assert extend_list([[0, 0, 1]], [[1, 0, 0]]) == [[0, 0, 1], [1, 0, 0]]
-
-def test_extend_3():
-    assert extend_list([[0, 0, 1], [0, 1, 0]], [[1, 0, 0], [0, 1, 0]]) == [[0, 0, 1], [0, 1, 0], [1, 0, 0]]
-
-def test_fail_1():
+def test_5():
     # Grid format:
     #   0 = Navigable space
     #   1 = Occupied space
 
     grid = [[0, 0, 1, 0, 0, 0],
             [0, 0, 1, 0, 0, 0],
-            [0, 0, 0, 0, 1, 1],
-            [0, 0, 1, 1, 1, 0],
-            [0, 0, 0, 0, 1, 0]]
-    init = [0, 0]
-    goal = [len(grid)-1, len(grid[0])-1]
-    cost = 1
-
-    assert search(grid, init, goal, cost) == 'fail'
-
-def test_fail_2():
-    # Grid format:
-    #   0 = Navigable space
-    #   1 = Occupied space
-
-    grid = [[0, 0, 1, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0],
-            [0, 0, 1, 0, 1, 1],
-            [0, 0, 1, 1, 1, 0],
+            [0, 0, 1, 0, 1, 0],
+            [0, 0, 1, 0, 1, 0],
             [0, 0, 1, 0, 1, 0]]
     init = [0, 0]
     goal = [len(grid)-1, len(grid[0])-1]
     cost = 1
 
-    assert search(grid, init, goal, cost) == 'fail'
+    print search(grid, init, goal, cost)
+
+def test_6():
+    # Grid format:
+    #   0 = Navigable space
+    #   1 = Occupied space
+
+    grid = [[0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 1, 0],
+            [0, 0, 1, 0, 1, 0],
+            [0, 0, 1, 0, 1, 0]]
+    init = [0, 0]
+    goal = [len(grid)-1, len(grid[0])-1]
+    cost = 1
+
+    print search(grid, init, goal, cost)
